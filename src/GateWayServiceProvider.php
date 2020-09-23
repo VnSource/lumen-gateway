09a7474907c7d5847f77e2caacf0ffeaf54b1168
daivietcom <63624549+daivietcom@users.noreply.github.com>
2020-09-23 09:03:29 +0700

    upload

---
+++ src/GateWayServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LumenGateWay;
+
+use Illuminate\Support\ServiceProvider;
+
+class GateWayServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
